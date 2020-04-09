25ded95dae9470f6f4180c13e0f50a71c6ac9e3e
ruchika <ruchika6698@gmail.com>
2020-04-09 17:05:34 +0530

    Added:Find Integer Maximum Number

--- MaximumNumber/MaximumNumber.cs
+++ MaximumNumber/MaximumNumber.cs
@@ -4,9 +4,29 @@ namespace Test_Maximum
 {
     class MaximumNumber
     {
+        public int maximumInteger(int firstValue, int secondValue, int thirdValue)
+        {
+            if(firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            else if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            else if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            else
+            {
+                return 0;
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Test Maximum Number");
+            MaximumNumber maximuminteger = new MaximumNumber();
         }
     }
 }
--- TestCase/TestMaximum.cs
+++ TestCase/TestMaximum.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using NUnit.Framework;
+using Test_Maximum;
 
 namespace Test_Maximum.TestCase
 {
+    [TestFixture]
     class TestMaximum
     {
+        [Test]
+        public void Max_ReturnsExpectedValue()
+        {
+            MaximumNumber md = new MaximumNumber();
+            int actualMax = md.maximumInteger(100, 225, 14);
+            Assert.AreEqual(225, actualMax);
+        }
+
     }
 }
