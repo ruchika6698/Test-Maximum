74c02ca40fce439c528ca6c25a52657734ec005c
ruchika <ruchika6698@gmail.com>
2020-04-09 17:23:55 +0530

    Added:Find Floating Maximum Number

--- MaximumNumber/MaximumNumber.cs
+++ MaximumNumber/MaximumNumber.cs
@@ -5,9 +5,36 @@ namespace Test_Maximum
 {
     class MaximumNumber
     {
+        ///<summary>
+        ///Method to get integer maximum number
+        /// </summary>
         public int maximumInteger(int firstValue, int secondValue, int thirdValue)
         {
             if(firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            else if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            else if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        ///<summary>
+        ///Method to get float maximum number
+        /// </summary>
+        public double doubleArray(double firstValue, double secondValue, double thirdValue)
+        {
+            double[] doubleNumber = { firstValue, secondValue, thirdValue };
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
             {
                 return firstValue;
             }
--- TestCase/TestMaximum.cs
+++ TestCase/TestMaximum.cs
@@ -13,5 +13,13 @@ namespace Test_Maximum.TestCase
             int actualMax = md.maximumInteger(100, 225, 14);
             Assert.AreEqual(225, actualMax);
         }
+
+        [Test]
+        public void Max_ReturnsExpectedValue1()
+        {
+            MaximumNumber md = new MaximumNumber();
+            double actualMax = md.doubleArray(10.5, 15.2, 14.3);
+            Assert.AreEqual(15.2, actualMax);
+        }
     }
 }
