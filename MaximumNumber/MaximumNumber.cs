a6c1f19f8bdd22268ef17ffaba78c550592e4e1f
ruchika <ruchika6698@gmail.com>
2020-04-09 16:32:36 +0530

    Added:Test Maximum Number

---
+++ MaximumNumber/MaximumNumber.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test_Maximum
+{
+    class MaximumNumber
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test Maximum Number");
+        }
+    }
+}
---
+++ TestCase/TestMaximum.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test_Maximum.TestCase
+{
+    class TestMaximum
+    {
+    }
+}
