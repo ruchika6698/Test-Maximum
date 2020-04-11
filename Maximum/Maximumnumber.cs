51b64c322add2266f194e736c9162abb87d866a0
ruchika <ruchika6698@gmail.com>
2020-04-11 17:46:09 +0530

    Added:Print Test Maximum

--- Maximum/Maximumnumber.cs
+++ Maximum/Maximumnumber.cs
@@ -5,9 +5,41 @@ namespace MaximumGeneric
 {
     public class MaximumNumber
     {
-        static void Main(string[] args)
+        //MAIN METHOD
+        public static void Main(String[] args)
         {
-            Console.WriteLine("Test Maximum Number");
+            Console.WriteLine("Welcome to Generics");
+            Console.ReadKey();
+        }
+        public class GenericClass<anyDataType>
+        {
+            //VARIABLE DECLARE
+            anyDataType element1, element2, element3;
+
+            //PARAMETERISED CONSTRUCTOR INITIALIZE VARIABLES
+            public GenericClass(anyDataType element1, anyDataType element2, anyDataType element3)
+            {
+                this.element1 = element1;
+                this.element2 = element2;
+                this.element3 = element3;
+            }
+
+            //METHOD CALLED INTERNALLY
+            public anyDataType testMaximum()
+            {
+                return testMaximum(element1, element2, element3);
+            }
+
+            //METHOD FOR FINDING MAXIMUM ELEMENT
+            public static anyDataType testMaximum(anyDataType element1, anyDataType element2, anyDataType element3)
+            {
+                //ARRAY DECALRE
+                anyDataType[] array = { element1, element2, element3 };
+
+                //FIND MAXIMUM ELEMENT
+                anyDataType MaximumValue = array.Max();
+                return MaximumValue;
+            }
         }
     }
 }
--- TestMaximum/UnitTest1.cs
+++ TestMaximum/UnitTest1.cs
@@ -1,9 +1,38 @@
 using NUnit.Framework;
 
-namespace TestMaximum
+namespace MaximumGeneric
 {
+    [TestFixture]
     public class Tests
     {
-        
+        ///<summary>
+        ///Test case for Integer maximum Number
+        /// </summary>
+        [Test]
+        public void Max_ReturnIntegerValue()
+        {
+            int actualMaxInteger = MaximumNumber.MaximumValue<int>.maxValueArray(100, 15, 154);
+            Assert.AreEqual(154, actualMaxInteger);
+        }
+
+        ///<summary>
+        ///Test case for Floating maximum Number
+        /// </summary>
+        [Test]
+        public void Max_ReturnFloatValue()
+        {
+            float actualMaxFloat = MaximumNumber.MaximumValue<float>.maxValueArray(10, 15, 14);
+            Assert.AreEqual(15, actualMaxFloat);
+        }
+
+        ///<summary>
+        ///Test case for String size maximum Number
+        /// </summary>
+        [Test]
+        public void Max_ReturnStringValue()
+        {
+            int actualMaxString = MaximumNumber.MaximumValue<string>.maxValueArray("Test".Length, "Maximum".Length, "Number".Length);
+            Assert.AreEqual(7, actualMaxString);
+        }
     }
 }
