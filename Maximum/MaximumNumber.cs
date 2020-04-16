7a83c685124a7384bd8bd6a70755a5b1430a908b
ruchika <ruchika6698@gmail.com>
2020-04-16 15:33:41 +0530

    Added:Find the Maximum Number More than three Parameter

---
+++ Maximum/MaximumNumber.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+
+namespace TestMaximum
+{
+    public class MaximumNumber
+    {
+        ///<summary>
+        ///Main Method
+        /// </summary>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Maximum Problem");
+        }
+        ///<summary>
+        ///GENERIC CLASS
+        /// </summary>
+        public class Generic<T> where T : IComparable
+        {
+            public T result;
+            private T data1;
+            private T data2;
+            private T data3;
+            private T temp;
+
+            public Generic(T variable1, T variable2, T variable3)
+            {
+                data1 = variable1;
+                data2 = variable2;
+                data3 = variable3;
+                result = testMaximum(data1, data2, data3);
+            }
+
+            public T testMaximum(T data1, T data2, T data3)
+            {
+
+                if (data1.CompareTo(data2) > 0 && data1.CompareTo(data3) > 0)
+                {
+                    return printMaximum(data1);
+                }
+                else if (data2.CompareTo(data1) > 0 && data2.CompareTo(data3) > 0)
+                {
+                    return printMaximum(data2);
+                }
+                else
+                {
+                    return printMaximum(data3);
+                }
+            }
+            public T printMaximum(T max)
+            {
+                Console.WriteLine("max value = " + max);
+                return max;
+            }
+        }
+        /// <summary>
+        /// Generic Class createg to check for more than 3 Data Entry
+        /// </summary>
+        public class GenericCheck<anyDataType>
+        {
+            public anyDataType Max(anyDataType[] values)
+            {
+                anyDataType[] Numbers = new anyDataType[values.Length];
+                for (int index = 0; index < values.Length; index++)
+                {
+                    Numbers[index] = values[index];
+                }
+                anyDataType MaximumValue = Numbers.Max();
+                return MaximumValue;
+            }
+        }
+    }
+}
+
---
+++ TestMaximum/UnitTest1.cs
@@ -0,0 +1,68 @@
+using NUnit.Framework;
+using static TestMaximum.MaximumNumber;
+
+namespace TestMaximum
+{
+    public class Tests
+    {
+        ///<summary>
+        ///Test case for Integer maximum Number
+        /// </summary>
+        [Test]
+        public void maximumNumberTest_toCompare_threeNumbers()
+        {
+            Generic<int> MaximumValueOfInteger = new Generic<int>(23, 43, 45);
+            Assert.AreEqual(45, MaximumValueOfInteger.result);
+        }
+        ///<summary>
+        ///Test case for Floating maximum Number
+        /// </summary>
+        [Test]
+        public void maximumDoubleTest_toCompare_threeNumbers()
+        {
+            Generic<float> MaximumValueOfFloat = new Generic<float>(2.6f, 9.5f, 5.6f);
+            Assert.AreEqual(9.5, MaximumValueOfFloat.result);
+        }
+        ///<summary>
+        ///Test case for String size maximum Number
+        /// </summary>
+        [Test]
+        public void maximumStringTest_toCompare_threeString()
+        {
+            Generic<int> MaximumValueOfString = new Generic<int>("The".Length, "Maximum".Length, "Number".Length);
+            Assert.AreEqual(7, MaximumValueOfString.result);
+        }
+        ///<summary>
+        ///print generic method to print to get maximum number
+        /// </summary>
+        [Test]
+        public static void maximumOfTheThreeIntegerFloatAndString()
+        {
+            Generic<int> MaximumValueOfInteger = new Generic<int>(23, 43, 45);
+            Generic<float> MaximumValueOfFloat = new Generic<float>(2.6f, 9.5f, 5.6f);
+            Generic<int> MaximumValueOfString = new Generic<int>("The".Length, "Maximum".Length, "Number".Length);
+            Assert.AreEqual(45, MaximumValueOfInteger.result);
+            Assert.AreEqual(9.5f, MaximumValueOfFloat.result);
+            Assert.AreEqual(7, MaximumValueOfString.result);
+        }
+        ///<summary>
+        ///More than three parameters
+        /// </summary>
+        [Test]
+        public void MorethanthreeParameters()
+        {
+            GenericCheck<int> obj1 = new GenericCheck<int>();
+            GenericCheck<double> obj2 = new GenericCheck<double>();
+            GenericCheck<int> obj3 = new GenericCheck<int>();
+            int[] Number1 = { 23, 43, 45, 55, 98, 60 };
+            double[] Number2 = { 0.5, 11.2, 20.9, 30.4, 70.8, 1.8 };
+            int[] Number3 = { "The".Length, "Maximum".Length, "Number".Length, "Problem".Length, "using".Length, "Generics".Length };
+            int result1 = (obj1.Max(Number1));
+            double result2 = (obj2.Max(Number2));
+            int result3 = (obj3.Max(Number3));
+            Assert.AreEqual(98, result1);
+            Assert.AreEqual(70.8, result2);
+            Assert.AreEqual(8, result3);
+        }
+    }
+}
